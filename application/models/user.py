09191f5c39bceee10e92e01287ad99cb0fc89ea6
leon mak <leonmakansheng@gmail.com>
2017-06-18 19:05:58 +0800

    Replace flask bcrypt with werkzeug

--- application/models/user.py
+++ application/models/user.py
@@ -1,5 +1,5 @@
-from index import db, bcrypt
-
+from index import db
+from werkzeug.security import generate_password_hash, check_password_hash
 
 class User(db.Model):
     id = db.Column(db.Integer(), primary_key=True)
@@ -13,12 +13,12 @@ class User(db.Model):
 
     @staticmethod
     def hashed_password(password):
-        return bcrypt.generate_password_hash(password)
+        return generate_password_hash(password)
 
     @staticmethod
     def get_user_with_email_and_password(email, password):
         user = User.query.filter_by(email=email).first()
-        if user and bcrypt.check_password_hash(user.password, password):
+        if user and check_password_hash(user.password, password):
             return user
         else:
             return None
